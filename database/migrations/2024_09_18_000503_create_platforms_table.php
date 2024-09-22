000bbca952633d436fb43ef4fd0a15a277823267
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-09-22 18:00:50 -0500

    add platform migration

---
+++ database/migrations/2024_09_18_000503_create_platforms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('platforms', function (Blueprint $table) {
+            $table->comment('Store platform information');
+            $table->id();
+            $table->string('name', 50)->nullable(false);
+            $table->string('description', 100)->nullable(true);
+            $table->dateTime('release_date')->nullable(true);
+            $table->string('logo', 255)->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->primary('id');
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('platforms');
+    }
+};
