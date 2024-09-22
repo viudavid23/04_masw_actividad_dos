12f39ef95023cdcedb2111e175a633ac20a5f325
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-09-22 18:03:23 -0500

    add constants and custom exceptions

---
+++ app/Exceptions/Constants.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Class that contains custom constants for exceptions
+ */
+class Constants{
+
+    const ERROR_CODE = 300;
+
+    const TXT_SUCCESS_CODE = 'Success';
+    const TXT_ERROR_CODE = 'Error';
+    const TXT_BAD_REQUEST_CODE = 'Bad Request';
+    const TXT_RECORD_SAVED = 'Record Saved';
+    const TXT_RECORD_UPDATED = 'Record Updated';
+    const TXT_RECORD_DELETED = 'Record Deleted';
+    const TXT_RECORD_NOT_FOUND_CODE = 'Record Not Found';
+    const TXT_RECORD_ALREADY_SAVED = 'Record already saved';
+    const TXT_RECORD_DOESNT_SAVED = 'Record does not saved yet';
+    const TXT_CANT_EXECUTE_OPERATION = 'Problem during the execution the operation';
+    const TXT_INVALID_ARGUMENT_CODE = "Invalid Arguments";
+    const TXT_FAILED_DEPENDENCY_CODE = "Failed Dependency";
+    const TXT_INTERNAL_SERVER_ERROR_CODE = "Internal Server Error";
+    const TXT_INVALID_PAGE_NUMBER = "Invalid Page Number";
+
+}
