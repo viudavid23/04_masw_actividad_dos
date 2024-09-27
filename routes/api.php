256e2ec7ea0e631041cd2f8ef293d7d3af432e7b
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-09-26 22:28:09 -0500

    Add actors routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\ActorController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\PlatformController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,14 @@ Route::prefix('platforms')->controller(PlatformController::class)->name('platfor
 });
 
 Route::prefix('languages')->controller(LanguageController::class)->name('languages.')->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::patch('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
+
+Route::prefix('actors')->controller(ActorController::class)->name('actors.')->group(function () {
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
     Route::post('/', 'store');
