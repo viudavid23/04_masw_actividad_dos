90844541c5e2c5b2a4a72518e29c578e1307f974
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-09-26 22:27:00 -0500

    Register actors and person contracts and services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Http\Contracts\ActorContract;
 use App\Http\Contracts\LanguageContract;
 use App\Http\Contracts\PlatformContract;
+use App\Http\Contracts\PersonContract;
+use App\Http\Services\Implementations\ActorService;
 use App\Http\Services\Implementations\LanguageService;
 use App\Http\Services\Implementations\PlatformService;
+use App\Http\Services\Implementations\PersonService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(ActorContract::class, ActorService::class);
         $this->app->bind(LanguageContract::class, LanguageService::class);
         $this->app->bind(PlatformContract::class, PlatformService::class);
+        $this->app->bind(PersonContract::class, PersonService::class);
     }
 
     /**
